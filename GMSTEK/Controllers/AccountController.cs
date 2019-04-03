0c5587edac4b359cb3c3d32a295949c2f54722c5
Marlon C.S <marlsas0306@gmail.com>
2019-04-02 21:40:39 -0500

    Adapted AccountController to DTO

--- GMSTEK/Controllers/AccountController.cs
+++ GMSTEK/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using GMSTEK.Models;
+using GMSTEK.DTO;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
